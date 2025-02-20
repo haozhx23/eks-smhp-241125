6c7e2d43cc53d74610a494e1f3a419ff81b52c8e
haozhx23 <121946073+haozhx23@users.noreply.github.com>
2025-02-20 20:43:45 +0800

    Update 5_check_cluster.sh

--- 5_check_cluster.sh
+++ 5_check_cluster.sh
@@ -3,3 +3,5 @@ kubectl auth can-i get deployments
 kubectl auth can-i get pytorchjobs.kubeflow.org
 
 aws sts get-caller-identity
+
+kubectl get nodes "-o=custom-columns=NAME:.metadata.name,INSTANCETYPE:.metadata.labels.node\.kubernetes\.io/instance-type,GPU:.status.allocatable.nvidia\.com/gpu,EFA:.status.allocatable.vpc\.amazonaws\.com/efa"
